8ef83e3770b06ea1d1bb45b1d7bbaf6187c2fc3c
exaucee <embuy098@uottawa.ca>
2024-04-03 03:10:48 -0400

    tous les fichiers

---
+++ about.php
@@ -0,0 +1,167 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>about us</h3>
+   <p> <a href="home.php">home</a> / about </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/about-img.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>At XauBooks, we offer a huge selection of books in all genres. From timeless classics to the latest bestsellers, we have something for every reader's taste and interest. We understand that waiting for your books can be frustrating. 
+            That's why we offer fast and reliable delivery options, so you can enjoy your new readings as soon as possible.</p>
+         
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="title">FEEDBACK</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/djenny.jpg" alt="">
+         <p>An efficient bookstore with all the books you are looking for I recommend you</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>DJENNY</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/kestia.jpg" alt="">
+         <p>Fast service, with all the books you want to order fast and always on time.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>KESTIA</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/esther.jpg" alt="">
+         <p>Fast and reliable book delivery service, ensuring timely access to a wide range of titles..</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>ESTHER</h3>
+      </div>
+
+     
+
+   </div>
+
+</section>
+
+<section class="authors">
+
+   <h1 class="title">AUTHORS</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/Birago_diop.png" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>BIRAGO DIOP</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/mongo_beti.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>MONGO BETI</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/Senghor.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3> LEOPOLD SEDAR SENGHOR</h3>
+      </div>
+
+      
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,48 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +1819-921-4516</p>
+       
+         <p> <i class="fas fa-envelope"></i> exauceemkabamba@gmail.com </p>
+         
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+        
+         
+        
+         
+         <a href=" https://github.com/exaucee2"> <i class="fab fa-github"></i> github </a>
+         <a href="https://www.linkedin.com/in/exaucee-mbuyi-b905b6275"> <i class="fab fa-linkedin"></i> linkedin </a>
+        
+      </div>
+     
+   
+
+   </div>
+
+   
+
+   
+
+</section>
